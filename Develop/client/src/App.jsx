9236e6cf072a4c7ae8ecaec46801b8c187b3c4d5
Donna Goon <g.donna120@gmail.com>
2023-11-14 10:50:39 -0600

    added queries and mutations to client

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Navbar from './components/Navbar';
 
 function App() {
+  const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache()
+  });
+
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
